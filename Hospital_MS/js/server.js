43ce65ac5073163d86a4da14deaaad2319aed721
MazharulCSE-wizard <mishafee.islam30@gmail.com>
2024-09-14 11:46:11 +0600

    changes

--- Hospital_MS/js/server.js
+++ Hospital_MS/js/server.js
@@ -8,6 +8,7 @@ const port= process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
+
 //Create database
 app.post('/doctor',(req,res)=>{
     var name = req.body.Dname;
@@ -18,17 +19,29 @@ app.post('/doctor',(req,res)=>{
     var password = req.body.Dpassword;
     var specialization = req.body.Specialization;
 
+    console.log("dbug",name);
+
     db.connect((err)=>{
         if (err) throw err;
 
-        var sql = "INSERT INTO HOSPITAL_MS(D_name,D_age,D_gender,D_mobile,D_email,D_password,Specialization) VALUES (?,?,?,?,?,?,?)";
-        const values = [name,age,gender.mobile,email,password,specialization]
+        
+        
+        // var sql = "INSERT INTO doctor (D_name,D_age,D_gender,D_mobile,D_email,D_password,Specialization) VALUES (?,?,?,?,?,?,?)";
+        
+        let sql = `INSERT INTO doctor ( D_name, D_age, D_gender, D_mobile, D_email, D_password, Specialization,H_id) VALUES (?,?,?,?,?,?,?)`
+        
+        const values = [name,age,"male",mobile,email,"password","heart"]
         db.query(sql,values, (err,res)=>{
             if(err) throw err;
             res.send("Account creation successful")
         })
     })
 
+})
+
+app.get("/home", (req,res)=>{
+    console.log("getting req")
+    res.send("hello world!")
 })
 
 app.listen(port, () => {
