ff1bff8239370c6ad41dc515ab2a1b7d05ea1005
MazharulCSE-wizard <mishafee.islam30@gmail.com>
2024-09-18 19:32:43 +0600

    new

--- Hospital_MS/js/server.js
+++ Hospital_MS/js/server.js
@@ -9,40 +9,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
 
-//Create database
-app.post('/doctor',(req,res)=>{
-    var name = req.body.Dname;
-    var age = req.body.Dage;
-    var gender = req.body.gender;
-    var mobile = req.body.Dmobile;
-    var email  =req.body.Demail;
-    var password = req.body.Dpassword;
-    var specialization = req.body.Specialization;
 
-    console.log("dbug",name);
-
-    db.connect((err)=>{
-        if (err) throw err;
-
-        
-        
-        // var sql = "INSERT INTO doctor (D_name,D_age,D_gender,D_mobile,D_email,D_password,Specialization) VALUES (?,?,?,?,?,?,?)";
-        
-        let sql = `INSERT INTO doctor ( D_name, D_age, D_gender, D_mobile, D_email, D_password, Specialization,H_id) VALUES (?,?,?,?,?,?,?)`
-        
-        const values = [name,age,"male",mobile,email,"password","heart"]
-        db.query(sql,values, (err,res)=>{
-            if(err) throw err;
-            res.send("Account creation successful")
-        })
-    })
-
-})
-
-app.get("/home", (req,res)=>{
-    console.log("getting req")
-    res.send("hello world!")
-})
 
 app.listen(port, () => {
     console.log(`Server running successfully on ${port}`)
